8dda93745dd12889ed938b79e12e0e1e7d210e1a
sreedharashwin <s223955271@deakin.edu.au>
2024-08-28 12:13:41 +1000

    Adding file

---
+++ factorial.py
@@ -0,0 +1,16 @@
+# Function to calculate factorial of a number
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Predefined number
+number = 5
+
+# Calculate factorial
+result = factorial(number)
+
+# Print result
+print(f"The factorial of {number} is {result}")
+
